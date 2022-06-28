f35e1d37fbc4d714aa2999eeeb94590e117b713a
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2022-06-28 11:07:11 -0300

    add changes in style

--- webapp/src/Components/Avatar/Index.jsx
+++ webapp/src/Components/Avatar/Index.jsx
@@ -4,7 +4,13 @@ const Avatar = () => {
 	return (
 		<div className="container_avatar">
 			<div className="avatar">
-				<div className="avatar_"></div>
+				<div className="avatar_">
+					<img
+						className="img"
+						src="https://th.bing.com/th/id/R.96f4799e78bbc5d3c2c54fad0bab73ce?rik=bYl2hAJtayyQ1A&pid=ImgRaw&r=0"
+						alt="react"
+					/>
+				</div>
 			</div>
 
 			<p>Gabriel Espinosa</p>
