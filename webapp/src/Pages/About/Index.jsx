31fb7adc89f6603d682c7f85e35c847a21bf8e59
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2022-07-13 10:05:04 -0300

    update about

--- webapp/src/Pages/About/Index.jsx
+++ webapp/src/Pages/About/Index.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -13,25 +15,27 @@ const lorem =
 const About = () => {
 	return (
 		<Layout>
-			<div className="about_picture">
-				<img
-					className="img"
-					src="https://th.bing.com/th/id/R.96f4799e78bbc5d3c2c54fad0bab73ce?rik=bYl2hAJtayyQ1A&pid=ImgRaw&r=0"
-					alt="react"
-				/>
-			</div>
-			<div className="about_content">
-				<p className="ac_title">
-					<span>Full Name</span> - Age
-				</p>
-				<span className="ac_job">Job</span>
-				<p className="ac_location">City - Country</p>
-				<p className="ac_descrition">{lorem}</p>
-			</div>
-			<div className="about_contact_list">
-				<ContactListItem Icon={MailOutlineIcon} />
-				<ContactListItem Icon={LinkedInIcon} />
-				<ContactListItem Icon={GitHubIcon} />
+			<div className="flex about_container">
+				<div className="about_picture">
+					<img
+						className="img"
+						src="https://th.bing.com/th/id/R.96f4799e78bbc5d3c2c54fad0bab73ce?rik=bYl2hAJtayyQ1A&pid=ImgRaw&r=0"
+						alt="react"
+					/>
+				</div>
+				<div className="about_content">
+					<p className="ac_title">
+						<span>Full Name</span> - Age
+					</p>
+					<span className="ac_job">Job</span>
+					<p className="ac_location">City - Country</p>
+					<p className="ac_descrition">{lorem}</p>
+				</div>
+				<div className="about_contact_list">
+					<ContactListItem Icon={MailOutlineIcon} />
+					<ContactListItem Icon={LinkedInIcon} />
+					<ContactListItem Icon={GitHubIcon} />
+				</div>
 			</div>
 		</Layout>
 	);
