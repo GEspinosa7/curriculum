182bd579de6455db43d5950054284feb52b49955
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2022-07-13 10:04:01 -0300

    update projects

--- webapp/src/Pages/Projects/Index.jsx
+++ webapp/src/Pages/Projects/Index.jsx
@@ -1,7 +1,50 @@
+import React from "react";
+
+import "./Style.css";
+
 import Layout from "../../Components/Layout/Index";
 
+import ProjectItem from "../../Components/ProjectItem/Index";
+
 const Projects = () => {
-	return <Layout />;
+	const projects = [
+		{
+			id: 1,
+			title: "Simple Store",
+			p_description: "lorem ipsum",
+			activities:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		},
+		{
+			id: 2,
+			title: "ChatBot Using Microsoft Azure",
+			p_description: "lorem ipsum",
+			activities:
+				"Quero Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		},
+		{
+			id: 3,
+			title: "ChatBot Using Microsoft Azure",
+			p_description: "lorem ipsum",
+			activities:
+				"Quero Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		},
+	];
+
+	return (
+		<Layout>
+			<div className="projects_container">
+				{projects.map((p) => (
+					<ProjectItem
+						id={p.id}
+						title={p.title}
+						description={p.p_description}
+						activities={p.activities}
+					/>
+				))}
+			</div>
+		</Layout>
+	);
 };
 
 export default Projects;
